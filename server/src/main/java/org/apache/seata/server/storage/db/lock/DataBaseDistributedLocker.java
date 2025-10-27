3cebf45cdf75bd49f9b09af3b698535be887753f
WangzJi <iwongjian666@gmail.com>
2025-10-27 15:24:19 +0800

    test: add some UT for core module

--- server/src/main/java/org/apache/seata/server/storage/db/lock/DataBaseDistributedLocker.java
+++ server/src/main/java/org/apache/seata/server/storage/db/lock/DataBaseDistributedLocker.java
@@ -233,9 +233,8 @@ public class DataBaseDistributedLocker implements DistributedLocker {
     }
 
     protected DistributedLockDO getDistributedLockDO(Connection connection, String key) throws SQLException {
-        try (PreparedStatement pst =
-                connection.prepareStatement(DistributedLockSqlFactory.getDistributedLockSql(dbType)
-                        .getSelectDistributeForUpdateSql(distributedLockTable))) {
+        try (PreparedStatement pst = connection.prepareStatement(DistributedLockSqlFactory.getDistributedLockSql(dbType)
+                .getSelectDistributeForUpdateSql(distributedLockTable))) {
 
             pst.setString(1, key);
             ResultSet resultSet = pst.executeQuery();
