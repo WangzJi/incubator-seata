fb261c972fc5aa3ac8f682cde0676c5cb8d758c3
WangzJi <iwongjian666@gmail.com>
2025-11-05 06:02:28 +0000

    test: add unit tests for server cluster module
    Add unit tests for cluster module components:
    - Message classes: RaftBaseMsg, RaftBranchSessionSyncMsg, RaftGlobalSessionSyncMsg, RaftClusterMetadataMsg, RaftVGroupSyncMsg
    - DTO classes: BranchTransactionDTO, GlobalTransactionDTO, RaftClusterMetadata
    - Serializer: JacksonSerializer
    - Event: ClusterChangeEvent
    
    Total: 10 new test files covering basic cluster module components

---
+++ server/src/test/java/org/apache/seata/server/cluster/raft/sync/msg/RaftVGroupSyncMsgTest.java
@@ -0,0 +1,79 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.seata.server.cluster.raft.sync.msg;
+
+import org.apache.seata.core.store.MappingDO;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class RaftVGroupSyncMsgTest {
+
+    @Test
+    public void testDefaultConstructor() {
+        RaftVGroupSyncMsg msg = new RaftVGroupSyncMsg();
+        assertNotNull(msg);
+        assertNull(msg.getMappingDO());
+        assertNull(msg.getMsgType());
+    }
+
+    @Test
+    public void testConstructorWithParameters() {
+        MappingDO mappingDO = new MappingDO("vgroup", "cluster");
+        RaftVGroupSyncMsg msg = new RaftVGroupSyncMsg(mappingDO, RaftSyncMsgType.ADD_VGROUP_MAPPING);
+
+        assertEquals(RaftSyncMsgType.ADD_VGROUP_MAPPING, msg.getMsgType());
+        assertEquals(mappingDO, msg.getMappingDO());
+    }
+
+    @Test
+    public void testSetAndGetMappingDO() {
+        RaftVGroupSyncMsg msg = new RaftVGroupSyncMsg();
+        MappingDO mappingDO = new MappingDO("vgroup2", "cluster2");
+        msg.setMappingDO(mappingDO);
+
+        assertEquals(mappingDO, msg.getMappingDO());
+    }
+
+    @Test
+    public void testSerialization() throws Exception {
+        MappingDO mappingDO = new MappingDO("test-vgroup", "test-cluster");
+        RaftVGroupSyncMsg original = new RaftVGroupSyncMsg(mappingDO, RaftSyncMsgType.REMOVE_VGROUP_MAPPING);
+
+        // Serialize
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        ObjectOutputStream oos = new ObjectOutputStream(bos);
+        oos.writeObject(original);
+        oos.flush();
+        byte[] serialized = bos.toByteArray();
+
+        // Deserialize
+        ByteArrayInputStream bis = new ByteArrayInputStream(serialized);
+        ObjectInputStream ois = new ObjectInputStream(bis);
+        RaftVGroupSyncMsg deserialized = (RaftVGroupSyncMsg) ois.readObject();
+
+        // Verify
+        assertEquals(original.getMsgType(), deserialized.getMsgType());
+        assertEquals(original.getMappingDO().getVGroup(), deserialized.getMappingDO().getVGroup());
+        assertEquals(original.getMappingDO().getCluster(), deserialized.getMappingDO().getCluster());
+    }
+}
