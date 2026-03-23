c1ac0ebdfabd89791faffe4b779a2b4b3fcac545
WangzJi <iwongjian666@gmail.com>
2026-03-23 11:54:29 +0800

    fix: UT failed

--- core/src/main/java/org/apache/seata/core/protocol/UnregisterRMRequest.java
+++ core/src/main/java/org/apache/seata/core/protocol/UnregisterRMRequest.java
@@ -21,6 +21,10 @@ package org.apache.seata.core.protocol;
  */
 public class UnregisterRMRequest extends AbstractIdentifyRequest {
 
+    public UnregisterRMRequest() {
+        this(null, null);
+    }
+
     public UnregisterRMRequest(String applicationId, String transactionServiceGroup) {
         super(applicationId, transactionServiceGroup);
     }
